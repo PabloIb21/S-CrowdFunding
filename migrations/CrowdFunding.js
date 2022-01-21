c172688b4ec315a809297b3cf5e81940a9cac712
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-01-20 22:52:51 -0600

    first commit

---
+++ migrations/CrowdFunding.js
@@ -0,0 +1,5 @@
+const CrowdFunding = artifacts.require("CrowdFunding"); //Instancia de nuestro contrato CrowdFunding.sol
+
+module.exports = function (deployer) {
+  deployer.deploy(CrowdFunding); //Este script hace deploy de nuestro contrato a la blockchain
+};
